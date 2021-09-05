bdf440595a0d2bbcd46ee29d9fdab19a96fe0ef7
kotil53 <osmankotil@outlook.com>
2021-09-05 22:36:45 +0300

    Python

---
+++ Pythonda Değişken Tanımlama/variables.py
@@ -0,0 +1,62 @@
+maasAli = 5000
+maasAhmet = 4000
+vergi = 0.27
+
+print(maasAli - (maasAli * vergi))
+print(maasAhmet - (maasAhmet * vergi))
+
+
+# Değişken Tanımlama KURALLARI
+
+# rakam ile başlayamaz
+
+number1 = 10
+print(number1)
+number1 = 20
+print(number1)
+
+number1 += 30
+print(number1)
+
+
+#Büyük küçük Harf Duyarlılığı vardır
+
+age = 20
+AGE = 30
+
+print(age)
+print(AGE)
+
+# Türkçe Karekter Kullanmayalım
+
+yas = 20
+_age = 20
+
+x = 1           # int
+y = 2.3         # float
+
+name = "Kotil"  # sting
+isStudent = True # bool
+
+
+# x, y, name, isStudent = (1, 2.3, "Kotil", True) #Tek satırda değişken Tanımlama
+
+ 
+a = "10" 
+b = "20"
+
+print(a+b) # 30 => 1020
+
+firstName = "Sadık"
+lastName = "Turan"
+
+print(firstName + lastName) # Sadık Turan 
+
+
+# Değişken tanımlarken aralarda boşluk bırakılmas
+
+
+
+
+
+
