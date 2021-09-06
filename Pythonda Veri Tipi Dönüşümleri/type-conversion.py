9aa3180fcb0c453e00f6e8e5c4551fc7f990763a
kotil53 <osmankotil@outlook.com>
2021-09-06 13:22:05 +0300

    Python

---
+++ Pythonda Veri Tipi Dönüşümleri/type-conversion.py
@@ -0,0 +1,58 @@
+"""
+x = input("1 sayı: ")
+y = input("2 sayı: ")
+
+print(type(x))
+print(type(y))
+toplam = int(x) + int(y)
+
+print(toplam)
+
+"""
+
+x = 5       #int
+y = 2.5     #float
+name = "Çınar"  #string
+isOnline = True #bool
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+
+# Type conversion
+
+# int to float
+
+# x = float(x)
+# print(x)
+# print(type(x))
+
+# y = int(y)
+# print(y)
+# print(type(y))
+
+# result = x + y
+# print(result)
+# print(type(result))
+
+
+
+# result = str(x) + str(y)
+# print(result)
+# print(type(result))
+
+# bool to str
+
+# isOnline = str(isOnline)
+# print(isOnline)
+# print(type(isOnline))
+
+
+# bool to int 
+
+isOnline = False 
+
+isOnline = int(isOnline)
+print(isOnline)
+print(type(isOnline))
---
+++ Veri Tipi Dönüşümleri/type_conversion_demo.py
@@ -0,0 +1,14 @@
+
+
+pi = 3.14
+
+r = float(input("Yarı çapı : "))
+alan = pi * (r ** 2)
+print(type(alan))
+
+cevre = 2 * pi * r
+print(type(cevre))
+
+print("Alan: "+ str(alan) + " çevre: " + str(cevre))18
+
+
